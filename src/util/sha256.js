10737b0496c3734ff579feb23a74e41071f3591f
YangKui <c6swift@gmail.com>
2018-01-21 16:43:55 +0800

    feat: 添加 sha256

---
+++ src/util/sha256.js
@@ -0,0 +1,23 @@
+function sha256(str) {
+  // We transform the string into an arraybuffer.
+  var buffer = new TextEncoder('utf-8').encode(str);
+  return (crypto.subtle || crypto.webkitSubtle)
+    .digest('SHA-256', buffer).then(hex);
+}
+
+function hex(buffer) {
+  let hexCodes = '';
+  const view = new DataView(buffer);
+  for (let i = 0; i < view.byteLength; i += 4) {
+    // Using getUint32 reduces the number of iterations needed (we process 4 bytes each time)
+    const value = view.getUint32(i);
+    // toString(16) will give the hex representation of the number without padding
+    const stringValue = value.toString(16);
+    // We use concatenation and slice for padding
+    const paddedValue = stringValue.padStart(8, '0');
+    hexCodes += paddedValue;
+  }
+
+  return hexCodes;
+}
+export default sha256;
