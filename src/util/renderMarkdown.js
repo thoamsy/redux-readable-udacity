36c5f2c6e0693db236a128108d40ad65dab8d56e
YangKui <c6swift@gmail.com>
2018-01-28 16:45:16 +0800

    feat: markdown 渲染函数

---
+++ src/util/renderMarkdown.js
@@ -0,0 +1,28 @@
+import Markdown from 'markdown-it';
+import hljs from 'highlight.js';
+
+const md = Markdown({
+  breaks: true,
+  linkify: true,
+  typographer: true,
+  highlight(str, lang) {
+    if (lang && hljs.getLanguage(lang)) {
+      return hljs.highlight(lang, str).value;
+    }
+    return '';
+  }
+});
+
+onmessage = ({ data }) => {
+  const { useFor } = data;
+  switch (useFor) {
+    case 'render': {
+      postMessage({
+        article: postMessage(md.render(data.markdown || ''))
+      });
+      break;
+    }
+    default:
+      break;
+  }
+};
