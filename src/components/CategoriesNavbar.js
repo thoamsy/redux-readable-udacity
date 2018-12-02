6a1e4dc6bf6c3f5c22e85ad3ddaac3bf15ccf75a
YangKui <c6swift@gmail.com>
2018-12-02 17:31:03 +0800

    refactor: 支持 NavLink

--- src/components/CategoriesNavbar.js
+++ src/components/CategoriesNavbar.js
@@ -3,12 +3,29 @@ import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { Link } from '@reach/router';
 
-const CategoryLink = styled(Link)`
+const NavLink = props => (
+  <Link
+    {...props}
+    getProps={({ isCurrent }) =>
+      isCurrent
+        ? {
+            style: {
+              color: 'rgba(0, 0, 0, 0.84)',
+              fontWeight: 600,
+            },
+          }
+        : null
+    }
+  />
+);
+
+const CategoryLink = styled(NavLink)`
   text-transform: uppercase;
   font-size: 18px;
   text-decoration: none;
   color: rgba(0, 0, 0, 0.56);
   transition: color ease-out 0.3s;
+
   &:hover {
     color: rgba(0, 0, 0, 0.84);
     text-decoration: underline;
@@ -31,11 +48,6 @@ const CategoriesItem = ({ categories }) =>
   categories.map(category => (
     <CategoryLink
       key={category.name}
-      exact
-      activeStyle={{
-        color: 'rgba(0, 0, 0, 0.84)',
-        fontWeight: 600,
-      }}
       className="navbar-item"
       to={'/' + category.path}
     >
